1ee6aa3e6c2a24fc00bd3388c2fff89caa7b5417
Lucas Corey <89759400+ArgentumRhodon@users.noreply.github.com>
2024-02-19 11:17:34 -0500

    created server, added Bootstrap, added client.html with nav

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const url = require('url');
+const clientHandler = require('./clientHandler');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  GET: {
+    '/': clientHandler.getIndex,
+    index: clientHandler.getIndex,
+  },
+  HEAD: {},
+  POST: {},
+};
+
+const onRequest = (request, response) => {
+  const parsedURL = url.parse(request.url);
+
+  const endpoint = urlStruct[request.method][parsedURL.pathname];
+  if (endpoint) {
+    return endpoint(request, response);
+  }
+  return urlStruct.GET.index(request, response);
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
