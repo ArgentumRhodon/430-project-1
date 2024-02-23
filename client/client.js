a986e76945b19534acac772f1ce1a19395f08938
ArgentumRhodon <lac2559@g.rit.edu>
2024-02-23 00:17:58 -0500

    added responsive mapbox

--- client/client.js
+++ client/client.js
@@ -1,1 +1,7 @@
-
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibHVjYXNjb3JleSIsImEiOiJjbG9xaWp2amowZ2VyMmxtdTJ0a2g1c2JpIn0.om_UI9UBSGsTmH9KM-8iaw";
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9, // starting zoom
+});
