066999567f545d303f414afdb8036fbe3d54e0f9
ArgentumRhodon <lac2559@g.rit.edu>
2024-02-23 16:01:01 -0500

    mapbox address autocomplete added

--- client/client.js
+++ client/client.js
@@ -5,3 +5,13 @@ const map = new mapboxgl.Map({
   center: [-74.5, 40], // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
+
+const script = document.createElement("script");
+script.src = "https://api.mapbox.com/search-js/v1.0.0-beta.18/web.js";
+script.onload = function () {
+  mapboxsearch.autofill({
+    accessToken:
+      "pk.eyJ1IjoibHVjYXNjb3JleSIsImEiOiJjbG9xaWp2amowZ2VyMmxtdTJ0a2g1c2JpIn0.om_UI9UBSGsTmH9KM-8iaw",
+  });
+};
+document.head.appendChild(script);
