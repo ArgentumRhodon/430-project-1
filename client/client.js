e2a32d3a949b6d68819e7f37cc8a1d20634d41ce
ArgentumRhodon <lac2559@g.rit.edu>
2024-03-03 22:57:41 -0500

    responsive changes

--- client/client.js
+++ client/client.js
@@ -1,27 +1,28 @@
-const locations = {};
+const locations = { address1: {}, address2: {} };
 
 // Sets up the logic for autofilling addresses
-mapboxgl.accessToken = 'pk.eyJ1IjoibHVjYXNjb3JleSIsImEiOiJjbG9xaWp2amowZ2VyMmxtdTJ0a2g1c2JpIn0.om_UI9UBSGsTmH9KM-8iaw';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibHVjYXNjb3JleSIsImEiOiJjbG9xaWp2amowZ2VyMmxtdTJ0a2g1c2JpIn0.om_UI9UBSGsTmH9KM-8iaw";
 
-const confirmRouteBtn = document.getElementById('confirmRoute');
+const confirmRouteBtn = document.getElementById("confirmRoute");
 
 // Initialize the autofill for address fields
 const initAutofill = () => {
-  const script = document.createElement('script');
-  script.src = 'https://api.mapbox.com/search-js/v1.0.0-beta.18/web.js';
+  const script = document.createElement("script");
+  script.src = "https://api.mapbox.com/search-js/v1.0.0-beta.18/web.js";
   script.onload = function () {
     const autofillCollection = mapboxsearch.autofill({
       accessToken:
-        'pk.eyJ1IjoibHVjYXNjb3JleSIsImEiOiJjbG9xaWp2amowZ2VyMmxtdTJ0a2g1c2JpIn0.om_UI9UBSGsTmH9KM-8iaw',
+        "pk.eyJ1IjoibHVjYXNjb3JleSIsImEiOiJjbG9xaWp2amowZ2VyMmxtdTJ0a2g1c2JpIn0.om_UI9UBSGsTmH9KM-8iaw",
     });
 
-    autofillCollection.addEventListener('retrieve', (e) => {
+    autofillCollection.addEventListener("retrieve", (e) => {
       const locationDetails = e.detail.features[0];
       // Takes 711 Kimball Drive to 711 Kimbal Drive to 711 Kimball Drive, Rochester, NY 14623
       const completeAddress = locationDetails.properties.full_address;
       // Mapbox's autofill is built for multi-input address forms, so this fills out
       // the whole address after Mapbox only inserts the street address.
-      e.target.addEventListener('change', () => {
+      e.target.addEventListener("change", () => {
         e.target.value = completeAddress;
         locations[e.target.id] = {};
         // Save coordinates of selected route
@@ -42,25 +43,25 @@ const initAutofill = () => {
 // Vehicle Stuff
 // Uses the FuelEconomy api to get increasingly specific options as part of a vehicle selection menu
 const getVehicleData = async (type, query) => {
-  const dataResponse = await fetch(`${type}${query ? `?${query}` : ''}`, {
-    method: 'GET',
+  const dataResponse = await fetch(`${type}${query ? `?${query}` : ""}`, {
+    method: "GET",
     headers: {
-      Accept: 'application/json',
+      Accept: "application/json",
     },
   });
   return dataResponse.json();
 };
 
 // Vehicle selection inputs
-const yearSelect = document.getElementById('yearSelect');
-const makeSelect = document.getElementById('makeSelect');
-const modelSelect = document.getElementById('modelSelect');
-const optionsSelect = document.getElementById('optionsSelect');
-const confirmVehicleBtn = document.getElementById('confirmVehicle');
+const yearSelect = document.getElementById("yearSelect");
+const makeSelect = document.getElementById("makeSelect");
+const modelSelect = document.getElementById("modelSelect");
+const optionsSelect = document.getElementById("optionsSelect");
+const confirmVehicleBtn = document.getElementById("confirmVehicle");
 
 // Order here is important
 const selectors = [yearSelect, makeSelect, modelSelect, optionsSelect];
-const defaults = ['Year', 'Make', 'Model', 'Options'];
+const defaults = ["Year", "Make", "Model", "Options"];
 
 // Fills a given select element with given options
 const fillSelect = (selectElement, options) => {
@@ -78,7 +79,7 @@ const fillSelect = (selectElement, options) => {
 };
 
 // A simple method that checks if a selection isn't the default option
-const isValidVehicleOption = (option) => !option.startsWith('Select');
+const isValidVehicleOption = (option) => !option.startsWith("Select");
 
 // Initializes the logic used by each vehicle selection input
 const initVehicleSelection = async () => {
@@ -89,7 +90,7 @@ const initVehicleSelection = async () => {
 
   // If a given selector is selected, account for change in other subsequent selectors
   for (let i = 0; i < selectors.length - 1; i++) {
-    selectors[i].addEventListener('change', (e) => {
+    selectors[i].addEventListener("change", (e) => {
       // If the given selector is invalid, also disable immediately subsequent selectors
       if (!isValidVehicleOption(e.target.value)) {
         confirmVehicleBtn.disabled = true;
@@ -111,50 +112,50 @@ const initVehicleSelection = async () => {
   }
 
   // Initialize the years input field
-  const years = await getVehicleData('year');
+  const years = await getVehicleData("year");
   fillSelect(yearSelect, years.menuItem);
   yearSelect.disabled = false;
 
   // Update functions for inputs, they use getVehicleData to fill based on previous selection value
   const updateMakes = async () => {
-    const makes = await getVehicleData('make', `year=${yearSelect.value}`);
+    const makes = await getVehicleData("make", `year=${yearSelect.value}`);
     fillSelect(makeSelect, makes.menuItem);
     makeSelect.disabled = false;
   };
   const updateModels = async () => {
     const models = await getVehicleData(
-      'model',
-      `year=${yearSelect.value}&make=${makeSelect.value}`,
+      "model",
+      `year=${yearSelect.value}&make=${makeSelect.value}`
     );
     fillSelect(modelSelect, models.menuItem);
     modelSelect.disabled = false;
   };
   const updateOptions = async () => {
     const options = await getVehicleData(
-      'options',
-      `year=${yearSelect.value}&make=${makeSelect.value}&model=${modelSelect.value}`,
+      "options",
+      `year=${yearSelect.value}&make=${makeSelect.value}&model=${modelSelect.value}`
     );
     fillSelect(optionsSelect, options.menuItem);
     optionsSelect.disabled = false;
   };
 
   // Add event listeners
-  yearSelect.addEventListener('change', (e) => {
+  yearSelect.addEventListener("change", (e) => {
     if (isValidVehicleOption(e.target.value)) {
       updateMakes();
     }
   });
-  makeSelect.addEventListener('change', (e) => {
+  makeSelect.addEventListener("change", (e) => {
     if (isValidVehicleOption(e.target.value)) {
       updateModels();
     }
   });
-  modelSelect.addEventListener('change', (e) => {
+  modelSelect.addEventListener("change", (e) => {
     if (isValidVehicleOption(e.target.value)) {
       updateOptions();
     }
   });
-  optionsSelect.addEventListener('change', (e) => {
+  optionsSelect.addEventListener("change", (e) => {
     if (isValidVehicleOption(e.target.value)) {
       confirmVehicleBtn.disabled = false;
     } else {
@@ -167,7 +168,7 @@ const initVehicleSelection = async () => {
 const confirmVehicle = async (e) => {
   e.preventDefault();
 
-  e.target.classList.add('was-validated');
+  e.target.classList.add("was-validated");
 
   const vehicleJSON = {
     year: yearSelect.value,
@@ -176,26 +177,26 @@ const confirmVehicle = async (e) => {
     options: optionsSelect.value,
   };
 
-  const response = await fetch('/vehicle', {
-    method: 'POST',
+  const response = await fetch("/vehicle", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
+      "Content-Type": "application/json",
+      Accept: "application/json",
     },
     body: JSON.stringify(vehicleJSON),
   });
 };
 
 // Route stuff
-const startInput = document.getElementById('address1');
-const endInput = document.getElementById('address2');
+const startInput = document.getElementById("address1");
+const endInput = document.getElementById("address2");
 
 // Sends the current route to the server, confirmation button is diabled by default
 const confirmRoute = async (e) => {
   e.preventDefault();
 
   // Visual form feedback
-  e.target.classList.add('was-validated');
+  e.target.classList.add("was-validated");
 
   // If both locations are validly selected from the auto-fill, query Mapbox for a route and send to the server
   // Only issue with this method is that perfect entry of a valid address does not work. You need to select from
@@ -203,16 +204,16 @@ const confirmRoute = async (e) => {
   if (locations.address1.coords && locations.address2.coords) {
     const query = await fetch(
       `https://api.mapbox.com/directions/v5/mapbox/driving/${locations.address1.coords[0]},${locations.address1.coords[1]};${locations.address2.coords[0]},${locations.address2.coords[1]}?geometries=geojson&access_token=${mapboxgl.accessToken}`,
-      { method: 'GET' },
+      { method: "GET" }
     );
     const json = await query.json();
 
     // Send route to the server
-    const response = await fetch('/route', {
-      method: 'POST',
+    const response = await fetch("/route", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
       body: JSON.stringify(json.routes[0]),
     });
@@ -220,14 +221,14 @@ const confirmRoute = async (e) => {
 };
 
 // Trip stuff
-const calculateTripButton = document.getElementById('calculateTrip');
+const calculateTripButton = document.getElementById("calculateTrip");
 
 // Converts a given number of seconds to "XX min" or "XX hr XX min"
 const secondsToTime = (seconds) => {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
 
-  let timeString = '';
+  let timeString = "";
 
   if (hours > 0) {
     timeString += `${hours} hr `;
@@ -240,22 +241,22 @@ const secondsToTime = (seconds) => {
 
 // Uses existsing information on the server to calculate the trip for currently selected information
 const calculateTrip = async () => {
-  const vehicleResponse = await fetch('/vehicle', {
-    method: 'GET',
+  const vehicleResponse = await fetch("/vehicle", {
+    method: "GET",
     headers: {
-      Accept: 'application/json',
+      Accept: "application/json",
     },
   });
-  const routeResponse = await fetch('/route', {
-    method: 'GET',
+  const routeResponse = await fetch("/route", {
+    method: "GET",
     headers: {
-      Accept: 'application/json',
+      Accept: "application/json",
     },
   });
-  const fuelPricesResponse = await fetch('/fuel', {
-    method: 'GET',
+  const fuelPricesResponse = await fetch("/fuel", {
+    method: "GET",
     headers: {
-      Accept: 'application/json',
+      Accept: "application/json",
     },
   });
   const vehicle = await vehicleResponse.json();
@@ -265,11 +266,11 @@ const calculateTrip = async () => {
   const vehicleProfileResponse = await fetch(
     `/vehicleProfile?id=${vehicle.options}`,
     {
-      method: 'GET',
+      method: "GET",
       headers: {
-        Accept: 'application/json',
+        Accept: "application/json",
       },
-    },
+    }
   );
   const vehicleProfile = await vehicleProfileResponse.json();
 
@@ -284,7 +285,7 @@ const calculateTrip = async () => {
   setTrip(vehicle, locations, totalTime, totalMiles, totalCost, vehicleImgURL);
 };
 
-const tripsSection = document.getElementById('trips');
+const tripsSection = document.getElementById("trips");
 // Updates the DOM with a given trip
 const setTrip = (vehicle, locations, time, miles, cost, vehicleImgURL) => {
   const html = `
@@ -334,14 +335,14 @@ const setTrip = (vehicle, locations, time, miles, cost, vehicleImgURL) => {
   `;
 
   // Create a save button with a working event
-  const saveButton = document.createElement('button');
-  saveButton.innerText = 'Save Trip';
-  saveButton.addEventListener('click', async () => {
-    const response = await fetch('/trip', {
-      method: 'POST',
+  const saveButton = document.createElement("button");
+  saveButton.innerText = "Save Trip";
+  saveButton.addEventListener("click", async () => {
+    const response = await fetch("/trip", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
       body: JSON.stringify({
         vehicle,
@@ -353,21 +354,21 @@ const setTrip = (vehicle, locations, time, miles, cost, vehicleImgURL) => {
       }),
     });
   });
-  saveButton.classList.add('btn', 'btn-info');
+  saveButton.classList.add("btn", "btn-info");
 
   // Update DOM
   tripsSection.innerHTML = html;
   tripsSection.appendChild(saveButton);
 };
 
-const getTripBtn = document.getElementById('getTrip');
+const getTripBtn = document.getElementById("getTrip");
 
 // Gets the currently saved trip from the server and populates the page with it
 const getTrip = async () => {
-  const tripResponse = await fetch('/trip', {
-    method: 'GET',
+  const tripResponse = await fetch("/trip", {
+    method: "GET",
     headers: {
-      Accept: 'application/json',
+      Accept: "application/json",
     },
   });
   const trip = await tripResponse.json();
@@ -377,27 +378,27 @@ const getTrip = async () => {
     trip.time,
     trip.miles,
     trip.cost,
-    trip.vehicleImgURL,
+    trip.vehicleImgURL
   );
 };
 
 // Call all initialization logic functions
 const init = () => {
-  startInput.value = '';
-  endInput.value = '';
+  startInput.value = "";
+  endInput.value = "";
 
   initAutofill();
   initVehicleSelection();
 
   confirmRouteBtn.disabled = true;
-  const vehicleForm = document.getElementById('vehicleForm');
-  const routeForm = document.getElementById('routeForm');
+  const vehicleForm = document.getElementById("vehicleForm");
+  const routeForm = document.getElementById("routeForm");
 
   // Add event listeners for forms and bottom two buttons
-  vehicleForm.addEventListener('submit', confirmVehicle);
-  routeForm.addEventListener('submit', confirmRoute);
-  calculateTripButton.addEventListener('click', calculateTrip);
-  getTripBtn.addEventListener('click', getTrip);
+  vehicleForm.addEventListener("submit", confirmVehicle);
+  routeForm.addEventListener("submit", confirmRoute);
+  calculateTripButton.addEventListener("click", calculateTrip);
+  getTripBtn.addEventListener("click", getTrip);
 };
 
 init();
