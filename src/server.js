c21a1ceb3ef25fc073d4911a6d34bfaeb7b6dc8e
ArgentumRhodon <lac2559@g.rit.edu>
2024-03-03 22:17:06 -0500

    resolved weird save conflict bug

--- src/server.js
+++ src/server.js
@@ -1,26 +1,26 @@
-const http = require('http');
-const url = require('url');
-const clientHandler = require('./clientHandler');
-const vehicleHandler = require('./vehicleResponse');
-const routeHandler = require('./routeHandler');
-const tripHandler = require('./tripHandler');
+const http = require("http");
+const url = require("url");
+const clientHandler = require("./clientHandler");
+const vehicleHandler = require("./vehicleResponse");
+const routeHandler = require("./routeHandler");
+const tripHandler = require("./tripHandler");
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const parseBody = (request, response, handler) => {
   const body = [];
 
-  request.on('error', (err) => {
+  request.on("error", (err) => {
     console.dir(err);
     response.statusCode = 400;
     response.end();
   });
 
-  request.on('data', (chunk) => {
+  request.on("data", (chunk) => {
     body.push(chunk);
   });
 
-  request.on('end', () => {
+  request.on("end", () => {
     const bodyString = Buffer.concat(body).toString();
     const bodyParams = JSON.parse(bodyString);
 
@@ -30,38 +30,38 @@ const parseBody = (request, response, handler) => {
 
 const urlStruct = {
   GET: {
-    '/': clientHandler.getIndex,
-    '/client.js': clientHandler.getJS,
-    '/vehicle': vehicleHandler.getVehicle,
-    '/route': routeHandler.getRoute,
-    '/year': vehicleHandler.getYears,
-    '/make': vehicleHandler.getMakes,
-    '/model': vehicleHandler.getModels,
-    '/options': vehicleHandler.getOptions,
-    '/fuel': vehicleHandler.getFuelPrices,
-    '/vehicleProfile': vehicleHandler.getVehicleProfile,
-    '/trip': tripHandler.getTrip,
+    "/": clientHandler.getIndex,
+    "/client.js": clientHandler.getJS,
+    "/vehicle": vehicleHandler.getVehicle,
+    "/route": routeHandler.getRoute,
+    "/year": vehicleHandler.getYears,
+    "/make": vehicleHandler.getMakes,
+    "/model": vehicleHandler.getModels,
+    "/options": vehicleHandler.getOptions,
+    "/fuel": vehicleHandler.getFuelPrices,
+    "/vehicleProfile": vehicleHandler.getVehicleProfile,
+    "/trip": tripHandler.getTrip,
     // "/getUsers": jsonHandler.getUsers,
     index: clientHandler.getIndex,
     notFound: vehicleHandler.notFound,
   },
   HEAD: {
-    '/vehicle': vehicleHandler.getVehicleMeta,
-    '/route': routeHandler.getRouteMeta,
-    '/trip': tripHandler.getTripMeta,
-    '/year': vehicleHandler.getYearsMeta,
-    '/make': vehicleHandler.getMakesMeta,
-    '/model': vehicleHandler.getModelsMeta,
-    '/options': vehicleHandler.getOptionsMeta,
-    '/fuel': vehicleHandler.getFuelPricesMeta,
-    '/vehicleProfile': vehicleHandler.getVehicleProfileMeta,
+    "/vehicle": vehicleHandler.getVehicleMeta,
+    "/route": routeHandler.getRouteMeta,
+    "/trip": tripHandler.getTripMeta,
+    "/year": vehicleHandler.getYearsMeta,
+    "/make": vehicleHandler.getMakesMeta,
+    "/model": vehicleHandler.getModelsMeta,
+    "/options": vehicleHandler.getOptionsMeta,
+    "/fuel": vehicleHandler.getFuelPricesMeta,
+    "/vehicleProfile": vehicleHandler.getVehicleProfileMeta,
     // "/getUsers": jsonHandler.getUsersMeta,
     notFound: vehicleHandler.notFoundMeta,
   },
   POST: {
-    '/vehicle': vehicleHandler.setVehicle,
-    '/route': routeHandler.setRoute,
-    '/trip': tripHandler.addTrip,
+    "/vehicle": vehicleHandler.setVehicle,
+    "/route": routeHandler.setRoute,
+    "/trip": tripHandler.addTrip,
     // "/addUser": jsonHandler.addUser,
     notFound: vehicleHandler.notFoundMeta,
   },
@@ -74,7 +74,7 @@ const onRequest = (request, response) => {
     return urlStruct.HEAD.notFound(request, response);
   }
 
-  if (request.method === 'POST') {
+  if (request.method === "POST") {
     if (urlStruct.POST[parsedURL.pathname]) {
       return parseBody(request, response, urlStruct.POST[parsedURL.pathname]);
     }
@@ -82,7 +82,7 @@ const onRequest = (request, response) => {
     return urlStruct[request.method][parsedURL.pathname](
       request,
       response,
-      parsedURL,
+      parsedURL
     );
   }
   return urlStruct[request.method].notFound(request, response);
